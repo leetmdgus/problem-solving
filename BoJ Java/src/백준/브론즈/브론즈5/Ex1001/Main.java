89ca47ee703eb7d815992ac69cea00561f5f9c86
leetmdgus <lee_tmdgus@naver.com>
2023-12-12 00:15:51 +0900

    add A-B file

---
+++ BoJ Java/src/백준/브론즈/브론즈5/Ex1001/Main.java
@@ -0,0 +1,18 @@
+package 백준.브론즈.브론즈5.Ex1001;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int result = A-B;
+        System.out.println(result);
+    }
+}
