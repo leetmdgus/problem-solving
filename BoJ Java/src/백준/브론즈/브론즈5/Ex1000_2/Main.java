c06e9fdb94a80b6690e4c93fe0507a2bc83389c7
leetmdgus <lee_tmdgus@naver.com>
2023-12-11 23:57:14 +0900

    change code A+B

--- BoJ Java/src/백준/브론즈/브론즈5/Ex1000_2/Main.java
+++ BoJ Java/src/백준/브론즈/브론즈5/Ex1000_2/Main.java
@@ -1,14 +1,18 @@
 package 백준.브론즈.브론즈5.Ex1000_2;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        int res = A+B;
-        System.out.println(res);
+        int result = A+B;
+        System.out.println(result);
     }
 }
