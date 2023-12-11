87c7278b4d41162bf0f86b648a3caa1e9451e008
leetmdgus <lee_tmdgus@naver.com>
2023-12-11 23:41:21 +0900

    add A+B problem

---
+++ BoJ Java/src/백준/브론즈/브론즈5/Ex1000_2/Main.java
@@ -0,0 +1,14 @@
+package 백준.브론즈.브론즈5.Ex1000_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int res = A+B;
+        System.out.println(res);
+    }
+}
