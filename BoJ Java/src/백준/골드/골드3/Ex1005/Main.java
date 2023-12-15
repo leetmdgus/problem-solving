e1cabb6918dc7c24f81c3cc9986da40a9706fb93
leetmdgus <lee_tmdgus@naver.com>
2023-12-15 11:54:27 +0900

    add topologicalSort problem and 1006 problem

--- BoJ Java/src/백준/골드/골드3/Ex1005/Main.java
+++ BoJ Java/src/백준/골드/골드3/Ex1005/Main.java
@@ -10,11 +10,10 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K, W;
-
-    static int[] edgeCnt;
-    static ArrayList<Integer>[] list;
     static int[] delay;
     static int[] maxDelay;
+    static int[] cntEdge;
+    static ArrayList<Integer>[] list;
 
     static StringBuilder sb = new StringBuilder();
 
@@ -24,11 +23,13 @@ public class Main {
 
         while (t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
             delay = new int[N + 1];
+            maxDelay = new int[N + 1];
+            cntEdge = new int[N + 1];
+
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++) {
                 delay[i] = Integer.parseInt(st.nextToken());
@@ -36,51 +37,45 @@ public class Main {
 
             list = new ArrayList[N + 1];
             for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
-            edgeCnt = new int[N + 1];
-            maxDelay = new int[N + 1];
 
-            for (int i = 0; i < K; i++) {
+            for (int i = 1; i <= K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
 
-                list[a].add(b);
-                edgeCnt[b]++;
+                list[x].add(y);
+                cntEdge[y]++;
             }
 
             W = Integer.parseInt(br.readLine());
             topologicalSort();
-
         }
+
         System.out.println(sb);
-
     }
-
-    static ArrayList<Integer> route = new ArrayList<>();
 
     public static void topologicalSort() {
         Queue<Integer> q = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            if (edgeCnt[i] == 0) {
+            if (cntEdge[i] == 0) {
                 q.add(i);
                 maxDelay[i] = delay[i];
             }
         }
 
         while (!q.isEmpty()) {
-            int now = q.poll();
-            route.add(now);
+            int now = q.remove();
 
             if (now == W) {
-                sb.append(maxDelay[W]).append('\n');
-                break;
+                sb.append(maxDelay[now]).append('\n');
+                return;
             }
 
             for (int next : list[now]) {
-                edgeCnt[next]--;
-                maxDelay[next] = Math.max(delay[next] + maxDelay[now], maxDelay[next]);
+                cntEdge[next]--;
+                maxDelay[next] = Math.max(maxDelay[now] + delay[next], maxDelay[next]);
 
-                if (edgeCnt[next] == 0) {
+                if (cntEdge[next] == 0) {
                     q.add(next);
                 }
             }
---
+++ BoJ Java/src/백준/플래티넘/플래티넘3/Ex1006/Main.java
@@ -0,0 +1,4 @@
+package 백준.플래티넘.플래티넘3.Ex1006;
+
+public class Main {
+}
